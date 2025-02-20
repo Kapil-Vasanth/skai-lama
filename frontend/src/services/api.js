858e4baeab181097f3d1d04e17029eb4a6487b09
kapil <kapilvasanthbalerao@gmail.com>
2025-02-20 16:53:48 +0530

    updating API URL for frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://skai-lama-swvr.onrender.com/api';
+// const API_URL = 'http://localhost:5000/api';
 
 // User services
 const register = async (userData) => {
